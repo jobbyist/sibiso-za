d103246c3b3caecf8b5a5e40d198487aad7b874a
jobbyist <232170229+jobbyist@users.noreply.github.com>
2026-06-14 20:01:30 +0200

    Update Logo.tsx

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,17 +1,17 @@
-import whiteLogo from "@/assets/sibiso-white.svg.asset.json";
-import blackLogo from "@/assets/sibiso-black.svg.asset.json";
+import whiteLogo from "@/assets/sibiso-white.svg";
+import blackLogo from "@/assets/sibiso-black.svg";
 
 export function Logo({ className = "", forceVariant }: { className?: string; forceVariant?: "light" | "dark" }) {
   if (forceVariant === "light") {
-    return <img src={whiteLogo.url} alt="Sibiso Marketing" width={150} height="auto" className={className} />;
+    return <img src={whiteLogo.url} alt="Sibiso Marketing" width={200} height="auto" className={className} />;
   }
   if (forceVariant === "dark") {
-    return <img src={blackLogo.url} alt="Sibiso Marketing" width={150} height="auto" className={className} />;
+    return <img src={blackLogo.url} alt="Sibiso Marketing" width={200} height="auto" className={className} />;
   }
   return (
     <>
-      <img src={blackLogo.url} alt="Sibiso Marketing" width={150} height="auto" className={`block dark:hidden ${className}`} />
-      <img src={whiteLogo.url} alt="Sibiso Marketing" width={150} height="auto" className={`hidden dark:block ${className}`} />
+      <img src={blackLogo.url} alt="Sibiso Marketing" width={200} height="auto" className={`block dark:hidden ${className}`} />
+      <img src={whiteLogo.url} alt="Sibiso Marketing" width={200} height="auto" className={`hidden dark:block ${className}`} />
     </>
   );
 }
